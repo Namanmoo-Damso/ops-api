27aede975db8bf20dcd6b6683545ed1660c67862
Ubuntu <ubuntu@ip-172-31-40-32.ap-northeast-2.compute.internal>
2026-01-01 06:18:47 +0000

    fix(rtc): RtcModule에 CallsModule import 추가

--- src/rtc/rtc.module.ts
+++ src/rtc/rtc.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RtcController } from './rtc.controller';
 import { RtcTokenService } from './rtc-token.service';
+import { CallsModule } from '../calls';
 
 @Module({
+  imports: [CallsModule],
   controllers: [RtcController],
   providers: [RtcTokenService],
   exports: [RtcTokenService],
